7605ad8588c6b82d0327c3c3c79f7e6e9d4b5bde
Vivek Vishwanath <vivs005@gmail.com>
2025-10-11 21:31:43 -0400

    Start P2

---
+++ project2/build.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+# Write a script to build your backend in this file 
+# (As required by your chosen backend language)
---
+++ project2/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Write a script to run your backend
+# This script must take exactly one terminal argument:
+# a path to an input IR file. It should also take one
+# flag, either ``--naive`` or ``--greedy`` to indicate
+# either the naive or intra-block greedy allocation algorithm.
+# This script should output a file ``out.s``.
+
+# For example, if a Tiger-IR file is located at path/to/file.ir,
+# then it should be possible to run run.sh as follows.
+
+# run.sh path/to/file.ir --naive
+# Produces out . s
+# run.sh path/to/file.ir --greedy
+# Produces out . s
